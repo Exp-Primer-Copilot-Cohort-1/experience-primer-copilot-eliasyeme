e5df68fe6d81f212430f5ccbb8d0a938b11682c8
eliasyeme <132422100+eliasyeme@users.noreply.github.com>
2023-12-13 10:38:12 +0000

    testing

---
+++ comment.js
@@ -0,0 +1,133 @@
+Synthesizing 10/10 solutions
+
+=======
+Suggestion 1
+
+function skillsMember() {
+    var member = {
+        name: "Hoàng Văn Tuyền",
+        age: 22,
+        skills: ["HTML", "CSS", "JavaScript", "ReactJS", "NodeJS", "PHP", "MySQL"]
+    };
+    var name = document.getElementById("name");
+    name.innerHTML = member.name;
+    var age = document.getElementById("age");
+    age.innerHTML = member.age;
+
+=======
+Suggestion 2
+
+function skillsMember() {
+    var member = {
+        name: "Nguyễn Văn A",
+        age: 20,
+        skills: ["Lập trình", "Chơi game", "Đọc sách"]
+    }
+
+=======
+Suggestion 3
+
+function skillsMember() {
+    var member = {
+        name: "Nguyễn Văn A",
+        age: 20,
+        skills: ["HTML", "CSS", "JS", "PHP"]
+    };
+
+=======
+Suggestion 4
+
+function skillsMember() {
+    var member = {
+        name: "Nguyen Thanh Tuan",
+        age: 23,
+        skills: ["HTML", "CSS", "JS", "PHP", "C#"]
+    };
+    var name = document.getElementById("name");
+    name.innerHTML = member.name;
+    var age = document.getElementById("age");
+    age.innerHTML = member.age;
+
+=======
+Suggestion 5
+
+function skillsMember() {
+    var member = {
+        name: "Nguyễn Văn A",
+        age: 25,
+        skills: [
+            "HTML",
+            "CSS",
+            "JS",
+            "PHP"
+        ]
+    };
+
+=======
+Suggestion 6
+
+function skillsMember() {
+    var member = {
+        name: "John",
+        skills: ["JavaScript", "CSS", "HTML"]
+    };
+    // var skills = document.getElementById("skills");
+    // var skillsMember = "";
+    // for (var i = 0; i < member.skills.length; i++) {
+    //     skillsMember += "<span>" + member.skills[i] + "</span>";
+    // }
+    // skills.innerHTML = skillsMember;
+    return member.skills;
+}
+
+=======
+Suggestion 7
+
+function skillsMember() {
+    var member = {
+        name: "Nguyễn Đức Mạnh",
+        age: 20,
+        skills: ["HTML", "CSS", "JS"]
+    };
+
+=======
+Suggestion 8
+
+function skillsMember() {
+    var member = {
+        name: "Nguyễn Văn A",
+        age: 20,
+        skills: ["HTML", "CSS", "JS"]
+    };
+
+=======
+Suggestion 9
+
+function skillsMember() {
+    // Path: member.js
+    var member = {
+        name: "Nguyễn Văn A",
+        age: 20,
+        skills: ["HTML", "CSS", "JS"],
+        avatar: "https://picsum.photos/200/200"
+    };
+    var name = document.getElementById("name");
+    name.innerText = member.name;
+    var age = document.getElementById("age");
+    age.innerText = member.age;
+    var avatar = document.getElementById("avatar");
+    avatar.src = member.avatar;
+
+=======
+Suggestion 10
+
+function skillsMember() {
+    var member = {
+        name: "Nguyen Van A",
+        age: 20,
+        skills: ["HTML", "CSS", "JS", "PHP", "C++"]
+    };
+    var name = document.getElementById("name");
+    name.innerHTML = member.name;
+    var age = document.getElementById("age");
+    age.innerHTML = member.age;
