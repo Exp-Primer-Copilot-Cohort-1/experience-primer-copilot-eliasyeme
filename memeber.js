0b667016040b3fbf45c67add7c825bbf794ea0a9
eliasyeme <132422100+eliasyeme@users.noreply.github.com>
2023-12-13 10:29:08 +0000

    Copilot second commit

--- memeber.js
+++ memeber.js
@@ -1,10 +1,8 @@
 function skillsMember() {
-    var member = document.getElementById("member");
     var skills = document.getElementById("skills");
-    var memberText = document.getElementById("member-text");
-    var skillsText = document.getElementById("skills-text");
-    if (member.style.display === "none") {
-        member.style.display = "block";
-        skills.style.display = "none";
+    var skillsMember = "";
+    for (var i = 0; i < member.skills.length; i++) {
+        skillsMember += "<span>" + member.skills[i] + "</span>";
     }
+    skills.innerHTML = skillsMember;
 }
